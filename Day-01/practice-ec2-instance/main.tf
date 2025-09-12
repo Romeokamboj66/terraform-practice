2a8b60ae962d85751c2d2672c20ee8ca98393b3c
Romeokamboj66 <Romeokamboj66@gmail.com>
2025-09-12 15:44:46 +0530

    Update main.tf

--- Day-01/practice-ec2-instance/main.tf
+++ Day-01/practice-ec2-instance/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
 region = "us-east-1" #set your desire AWS region
 }
 resource "aws_instance" "example" {
-ami = "ami-0c55b159cbfafe1f0"  # Specify an appropriate AMI ID
+ami = "ami-0360c520857e3138f"  # Specify an appropriate AMI ID
 instance_type = "t2.micro"
 }
 
